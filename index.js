4a3bff54cd8e330e7fa201de4b8e5156d2efc63b
mevipro123456 <mevipro123456@gmail.com>
2022-11-23 15:09:14 +0700

    update index

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const db = require('../models/queries')
+const db = require('./models/queries')
 const port = 3000
 
 app.use(bodyParser.json())
