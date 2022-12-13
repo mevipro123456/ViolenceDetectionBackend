910d6f230415c93a89a462a4326b264c6b008f14
Bui Duc Nhan <nhanbuiduc.work@gmail.com>
2022-12-13 14:03:18 +0700

    update subcription

--- modules/subcription.js
+++ modules/subcription.js
@@ -79,7 +79,7 @@ const createSubcription = async (request, response) => {
     var duration = service[0].duration
     var start_date = new Date()
     var end_date = new Date()
-    end_date.setMonth(start_date.getMonth() + 12)
+    end_date.setMonth(start_date.getMonth() + duration)
     console.log("Current start_date ", start_date.toISOString())
     console.log("Current end_date ", end_date.toISOString())
     pool.query('INSERT INTO subcription (start_date, end_date, price, duration, account_id, service_id) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *', [start_date.toISOString(), end_date.toISOString(), price, duration, account_id, service_id], (error, results) => {
